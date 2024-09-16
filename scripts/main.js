6c38600d979bb409b3f7697f3d1b7f6a8ae39324
chikien111 <38467294+chikien111@users.noreply.github.com>
2024-09-16 13:28:57 +0700

    update page

--- scripts/main.js
+++ scripts/main.js
@@ -26,19 +26,36 @@ jQuery(document).on("ready",function () {
 
 new WOW().init();
 
-$('.owl-carousel').owlCarousel({
-  center:true,
-    loop:true,
-    margin:10,
-    responsive:{
-        0:{
-            items: 2
-        },
-        600:{
-            items: 2
-        },
-        1000:{
-            items: 2
-        }
-    }
-})
+// $('.owl-carousel').owlCarousel({
+//   center:true,
+//     loop:true,
+//     margin:10,
+//     responsive:{
+//         0:{
+//             items: 2
+//         },
+//         600:{
+//             items: 2
+//         },
+//         1000:{
+//             items: 2
+//         }
+//     }
+// })
+var owl = $('.owl-carousel');
+
+owl.children().each( function( index ) {
+  $(this).attr( 'data-position', index ); // NB: .attr() instead of .data()
+});
+
+owl.owlCarousel({
+  center: true,
+  loop: true,
+  items: 2,
+});
+
+$(document).on('click', '.owl-item>div', function() {
+  // see https://owlcarousel2.github.io/OwlCarousel2/docs/api-events.html#to-owl-carousel
+  var $speed = 500;  // in ms
+  owl.trigger('to.owl.carousel', [$(this).data( 'position' ), $speed] );
+});
